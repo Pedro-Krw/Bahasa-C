3f5c08cb813c4f23c99cb4a26efdb68252dbb896
pedrokorwa <korwapedro@gmail.com>
2020-06-16 09:27:23 +0700

    update 9:27

---
+++ C,c++1/new.cpp
@@ -0,0 +1,18 @@
+
+#include "stdio.h"
+
+main (){
+	int sa,ko;
+	 printf("masuka nilai:");
+	  scanf ("%d",&sa);
+	  
+	  if (sa < 60){
+	  	printf("ANDA TIDAK LULUS");
+	  }
+	  else {
+	  	printf("LULUS");
+	  }
+			
+		
+	   }
+	
